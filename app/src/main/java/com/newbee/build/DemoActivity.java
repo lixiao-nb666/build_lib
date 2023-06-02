dc864a8b1982adebea89fc59ea435368c90922fb
lixiao <lixiao@nb-666.com>
2023-06-02 15:19:31 +0800

    修改

--- app/src/main/java/com/newbee/build/DemoActivity.java
+++ app/src/main/java/com/newbee/build/DemoActivity.java
@@ -1,14 +1,18 @@
 package com.newbee.build;
 
 import android.Manifest;
+import android.view.View;
+import android.widget.TextView;
 
 import com.newbee.bulid_lib.mybase.activity.welcome.BaseWelcomeActivity;
 import com.newbee.bulid_lib.mybase.activity.welcome.bean.WelcomeInfoBean;
+import com.newbee.bulid_lib.util.AudioUtil;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DemoActivity extends BaseWelcomeActivity {
+    private TextView tv;
 
     @Override
     public int getWelcomeLayoutId() {
@@ -17,7 +21,15 @@ public class DemoActivity extends BaseWelcomeActivity {
 
     @Override
     public void initWelcomeView() {
+        tv=findViewById(R.id.tvTitle);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int nowNumb=AudioUtil.getInstance().getVolume();
 
+                AudioUtil.getInstance().setVolume(nowNumb+1);
+            }
+        });
     }
 
     @Override
@@ -50,6 +62,8 @@ public class DemoActivity extends BaseWelcomeActivity {
     @Override
     public void userGetAllPermission() {
         showToast("get all permission");
-        finish();
+        AudioUtil.getInstance().init(context);
+
+//        finish();
     }
 }
--- mylibrary/src/main/java/com/newbee/bulid_lib/util/AudioUtil.java
+++ mylibrary/src/main/java/com/newbee/bulid_lib/util/AudioUtil.java
@@ -2,9 +2,7 @@ package com.newbee.bulid_lib.util;
 
 import android.content.Context;
 import android.media.AudioManager;
-import android.text.TextUtils;
-import com.newbee.bulid_lib.mybase.appliction.BaseApplication;
-import com.newbee.bulid_lib.mybase.share.MyShare;
+
 
 
 /**
@@ -17,12 +15,6 @@ public class AudioUtil {
     private static AudioUtil mInstance;
 
     private AudioUtil() {
-        try {
-            mAudioManager = (AudioManager) BaseApplication.getContext().getSystemService(Context.AUDIO_SERVICE);
-        }catch (Exception e){
-            mAudioManager=null;
-        }
-
     }
 
     public synchronized static AudioUtil getInstance() {
@@ -32,10 +24,24 @@ public class AudioUtil {
         return mInstance;
     }
 
+    public void init(Context context){
+        mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+    }
+
+
+
+    //AudioManager.STREAM_VOICE_CALL
+    private int audioType=AudioManager.STREAM_MUSIC;
+
+
+    public void setAudioType(int setType){
+        this.audioType=setType;
+    }
+
     //获取多媒体最大音量
-    public int getMediaMaxVolume() {
+    public int getMaxVolume() {
         try {
-            return mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+            return mAudioManager.getStreamMaxVolume(audioType);
         }catch (Exception e){
             return -1;
         }
@@ -43,104 +49,40 @@ public class AudioUtil {
     }
 
     //获取多媒体音量
-    public int getMediaVolume() {
+    public int getVolume() {
         try {
-            return mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+            return mAudioManager.getStreamVolume(audioType);
         }catch (Exception e){
             return -1;
         }
-
     }
 
-    //获取通话最大音量
-    public int getCallMaxVolume() {
-        try {
-            return mAudioManager.getStreamMaxVolume(AudioManager.STREAM_VOICE_CALL);
-        }catch (Exception e){
-            return -1;
-        }
 
-    }
 
-    //获取系统音量最大值
-    public int getSystemMaxVolume() {
-        try {
-            return mAudioManager.getStreamMaxVolume(AudioManager.STREAM_SYSTEM);
-        }catch (Exception e){
-            return -1;
-        }
-
-    }
-
-    //获取系统音量
-    public int getSystemVolume() {
-        try {
-            int maxNumb = getSystemMaxVolume();
-            int nowNumb = mAudioManager.getStreamVolume(AudioManager.STREAM_SYSTEM);
-            int showNumb=nowNumb * 100 / maxNumb;
-            String shareStr= MyShare.getInstance().getString(systemVolumeKey);
-            if(!TextUtils.isEmpty(shareStr)){
-                int share=Integer.valueOf(shareStr);
-               int cha= Math.abs(share - showNumb);
-               if(cha<17){
-                   showNumb=share;
-               }
-            }
-
-            return showNumb;
-        }catch (Exception e){
-            return -1;
-        }
-
-    }
-
-    public int getVoiceNumb() {
-        try {
-            int maxNumb = getSystemMaxVolume();
-
-            int nowNumb = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-            return nowNumb * 100 / maxNumb;
-        }catch (Exception e){
-            return -1;
-        }
-
-    }
-
-    //获取提示音量最大值
-    public int getAlermMaxVolume() {
-        return mAudioManager.getStreamMaxVolume(AudioManager.STREAM_ALARM);
-    }
 
     /**
      * 设置多媒体音量
      * 这里我只写了多媒体和通话的音量调节，其他的只是参数不同，大家可仿照
      */
-    private final String systemVolumeKey="SystemVolume";
-    public void setSystemVolume(int volume) {
+    public void setVolume(int volume) {
         try {
-            int needVolume = getSystemMaxVolume();
-            if (volume <= 0) {
-                needVolume = 0;
-            } else if (volume > 0 && volume < 100) {
-                needVolume = needVolume * volume / 100;
-            }
-            mAudioManager.setStreamVolume(AudioManager.STREAM_SYSTEM, //音量类型
-                    needVolume,
+
+            mAudioManager.setStreamVolume(audioType, //音量类型
+                    volume,
                     AudioManager.FLAG_PLAY_SOUND
                             | AudioManager.FLAG_SHOW_UI);
-            MyShare.getInstance().putString(systemVolumeKey,volume+"");
         } catch (Exception e) {
         }
-
     }
+
 
     /**
      * 设置多媒体音量
      * 这里我只写了多媒体和通话的音量调节，其他的只是参数不同，大家可仿照
      */
-    public void setMediaVolume(int volume) {
+    public void setVolumeByPercentage(int volume) {
         try {
-            int needVolume = getMediaMaxVolume();
+            int needVolume = getMaxVolume();
             if (volume <= 0) {
                 needVolume = 0;
             } else if (volume > 0 && volume < 100) {
@@ -155,15 +97,8 @@ public class AudioUtil {
         }
     }
 
-    //设置通话音量
-    public void setCallVolume(int volume) {
-        try {
-            mAudioManager.setStreamVolume(AudioManager.STREAM_VOICE_CALL,
-                    volume,
-                    AudioManager.STREAM_VOICE_CALL);
-        }catch (Exception e){}
 
-    }
+
 
     // 关闭/打开扬声器播放
     public void setSpeakerStatus(boolean on) {
